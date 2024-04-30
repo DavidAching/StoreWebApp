62634c087b9963a774e29be7b88bb3a12afb3543
David Aching <daviddjdubstep@gmail.com>
2024-04-29 20:14:13 -0500

    dinamismo en subtotal

--- scripts/productDetail.js
+++ scripts/productDetail.js
@@ -3,20 +3,25 @@ const params = new URLSearchParams(query);
 const id = params.get("id");
 console.log(id);
 const product = products.find((each) => each.id === id);
-console.log(product);
+console.log(product.price);
 
 
+
+
+// console.log(products.images.id);
+
+/* #region  FUNCION DE IMPRESION DE LOS DATOS DE PRODUCTO EN HTML DESDE JAVASCRIPT */
 function printDetails(id) {
   const product = products.find((each) => each.id === id);
   const detailsTemplate = `
   <div class="product-images-block">
   <div class="product-images-little">
-    ${product.images.map(
-        each => `<img src="${each}" width="50px alt="mini" />`
-    ).join("")}
+    ${product.images
+      .map((each) => `<img onclick="changeMini(this)" src="${each}" width="50px alt="mini" />`)
+      .join("")}
   </div>
   <div class="product-image">
-    <img src="${product.img}" width="300px" height="280px" alt="">
+    <img id="bigImg" src="${product.img}" width="300px" height="280px" alt="">
   </div>
 </div>
 <div class="product-description-block">
@@ -25,9 +30,9 @@ function printDetails(id) {
     <fieldset>
         <label class="label" for="color">Color</label>
         <select type="text" placeholder="Selecciona un color">
-         ${product.colors.map(
-            (each) => `<option value=${each}>${each}</option>`
-         ).join("")}
+         ${product.colors
+           .map((each) => `<option value=${each}>${each}</option>`)
+           .join("")}
       </select>
     </fieldset>
   </form>
@@ -42,7 +47,7 @@ function printDetails(id) {
 <div class="product-checkout-block">
   <div class="product-checkout-container">
     <p class="total">Total:</p>
-    <h2 class="finalprice">${product.price}</h2>
+    <h2 class="finalprice">S/.${product.price}</h2>
     <p class="impuestos">Incluye Impuesto PAIS y percepción AFIP. Puedes recuperar PEN S/.50711 haciendo la solicitud en AFIP.</p>
     <div class="opciones-extras">
       <div class="op1">
@@ -55,42 +60,39 @@ function printDetails(id) {
       </div>
     </div>
     <div class="add-cart">
-      <input type="number" name="" id="" placeholder="1">
+      <input type="number" name="" onchange="changeSubtotal(this, ${product.price})" placeholder="1">
       <button type="button">Añadir al carrito</button>
+    </div>
+    <div class="add-cart" id="subTotal">
+        <div class="add-cart">
+          <p>El total es : <span id="precioFinal"> </span> </p>
+        </div>
     </div>
   </div>
 </div> 
     `;
   const detailsSelector = document.querySelector("#detail-container");
   detailsSelector.innerHTML = detailsTemplate;
+
 }
+/* #endregion */
 
 printDetails(id);
 
 
+function changeMini(item) {
+  const selectedSrc = item.src;
+  const bigSelector = document.querySelector("#bigImg");
+  bigSelector.src = selectedSrc;
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+function changeSubtotal (input, precio) {
+  totalCompra = input.value;
+  console.log(totalCompra);
+  subTotal = precio * totalCompra;
+  console.log(subTotal);
+  refreshScreen = document.querySelector("#precioFinal")
+  console.log(refreshScreen);
+  refreshScreen.textContent = subTotal;
+}
